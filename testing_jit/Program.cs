84a18963701865f45a58530b509f71b993e9ef31
kn0 <vladvatafu9@gmail.com>
2023-04-24 04:06:10 +0300

    Add files via upload
    Trying to see if we can spray jit to bypass dep in c#. Help idk what i do :))
    Until now tried https://rh0dev.github.io/blog/2017/the-return-of-the-jit/
    will try to implement when feeling a little bit better https://bugs.chromium.org/p/chromium/issues/detail?id=48933
    to see if we can get it work 
    Code in this repo rn is just for the rh0dev impelementation in c#

---
+++ testing_jit/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+
+internal class Program
+{
+    private static Func<long> asm_js_module()
+    {
+        Func<long> payload_code = new Func<long>(() =>
+        {
+            long val = 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa8909090) | 0;
+            val = (val + 0xa89090c3) | 0;
+            return val | 0;
+        });
+        return payload_code;
+    }
+
+    private static void spray()
+    {
+        List<Func<long>> sprayed = new List<Func<long>>();
+        for (int i = 0; i <= 0x1800; i++)
+        {
+            sprayed.Add(asm_js_module());
+        }
+    }
+
+  
+
+    private static void Main(string[] args)
+    {
+        spray();
+
+        Stopwatch sw = Stopwatch.StartNew();
+        for (int index = 0; index < 0x10000; index++)
+            spray();
+        for (int index = 0; index < 0x10000; index++)
+            spray();
+        for (int index = 0; index < 0x10000; index++)
+            spray();
+        for (int index = 0; index < 0x10000; index++)
+            spray();
+        sw.Stop();
+    }
+}
